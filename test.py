0f6b59f44a4f7a37e1d94398d2877871ebafd818
Ayan Sheikh <ayansheikh52@gmail.com>
2025-03-30 12:22:56 +0530

    remove test file and update .gitignore and requirements

--- test.py
+++
@@ -1,9 +0,0 @@
-from .story_cli_groq import initialize_groq_client
-
-
-def test_initialize_groq_client():
-    # Test the Groq client initialization
-    client = initialize_groq_client()
-
-    print(client
-    )
